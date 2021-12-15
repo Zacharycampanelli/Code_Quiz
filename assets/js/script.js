b55e256388a509a77f15b5b90232f942dc6927db
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2021-12-15 00:36:10 -0500

    changed organization of project so that rather than multiple html pages they are dynamically changed. This is just a test to play with DOM Manipulation q :q q
    2;
    1e;12
    ;2
    ;c Q:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
-// Assignment code
-var nextPageButton = document.querySelector(".next_page");
+var questionEl = document.querySelector("#question_h1")
+var option1El =  document.querySelector("#q1");
+var option2El =  document.querySelector("#q2");
+var option3El =  document.querySelector("#q3");
+var option4El =  document.querySelector("#q4");
+var pEl = document.querySelector("p");
+var mainEl = document.querySelector(".main")
+var beginBtnEl = document.querySelector("#begin");
+var quizEl = document.querySelector("#quiz")
+// // Question 1
+// questionEl.textContent =
+// option1El.textContent = 
+
+function startGame() {
+    
+    var choicesEl = document.createElement("ul");
+    console.log(choicesEl)
+    quizEl.appendChild(choicesEl);
+    var opt1El = document.createElement("li");
+    console.log(opt1El);
+    opt1El.innerHTML = "hi";
+    choicesEl.appendChild(opt1El);
+    questionEl.innerHTML = "1"
+    
+}
 
 
 
+beginBtnEl.addEventListener("click", function(){
+    pEl.remove();
+    beginBtnEl.remove()
+    startGame();
+});
 
-
-
-// Add event listener
-nextPageButton.addEventListener("click", )
+    
