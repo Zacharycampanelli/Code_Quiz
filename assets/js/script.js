13280d56ae142eb7bad453d8ad85739d2376dadb
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2021-12-19 19:53:14 -0500

    most basic functionality working besides sorting high scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,33 @@
 var mainEl = document.querySelector(".main");
 var h1El = document.createElement("h1");
+var startPageEl = document.createElement("div");
 var questionEl = document.createElement("div");
+var scoreSubmitEl = document.createElement("div");
+var highScorePageEl = document.createElement("div");
+
 var pEl = document.createElement("p");
+
+var yourScoreEl = document.createElement("div");
 // var quizEl = document.querySelector("#quiz");
-// var beginBtnEl = document.querySelector("#begin");
+var beginBtnEl = document.createElement("button");
 // var pEl = document.createElement("p");
 
 var opt1El = document.createElement("button");
 var opt2El = document.createElement("button");
 var opt3El = document.createElement("button");
 var opt4El = document.createElement("button");
-var optionContainerEL = document.createElement("div");
+// var optionContainerEL = document.createElement("div");
 
 var footerEl = document.querySelector("footer");
+var enterScoreFormEl = document.createElement("form");
+var inputFieldEl = document.createElement("input");
+var submitButtonEl = document.createElement("button");
+var goBackButtonEl = document.createElement("button");
+var clearScores = document.createElement("button");
+
+var scoreInfoEl = document.createElement("div");
+var scoreItemEl = document.createElement("li");
+var scoreSheetEl = document.createElement("ol");
 
 var questionNumber = 0;
 var check;
@@ -20,6 +35,8 @@ var check;
 var timer = 30;
 
 var timerEl = document.querySelector("#time");
+var highScores = [];
+var scoreNumber = 0;
 
 var quizQuestions = [
   {
@@ -74,44 +91,132 @@ function scoreFormHandler(event) {
   createScore(userScores);
 }
 
-function clearPage() {
-  while (mainEl.hasChildNodes()) {
-    mainEl.removeChild(mainEl.firstChild);
-  }
+function createScore(userScores) {
+  scoreItemEl.className = "score-item";
+  scoreItemEl.setAttribute("data-score-id", scoreNumber);
+
+  scoreInfoEl.className = "score-list";
+  scoreInfoEl.innerHTML =
+    "<h2 class = 'score-name'>" +
+    userScores.initials +
+    "</h3><span class='user-score'>" +
+    userScores.score +
+    "</span>";
+
+  loadScores();
+
+  userScores.id = scoreNumber;
+  scoreNumber++;
+
+  highScores.push(userScores);
+
+  saveScore();
+
+  buildHighScorePage(userScores.score);
+}
+
+function hide(item) {
+  item.setAttribute("class", "hide");
+}
+
+function buildStartPage() {
+  debugger;
+  highScorePageEl.classList.add("hide");
+  startPageEl.classList.remove("hide");
+  h1El.textContent = "Coding Quiz Challenge";
+  pEl.textContent =
+    "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+  startPageEl.appendChild(h1El);
+  startPageEl.appendChild(pEl);
+
+  beginBtnEl.setAttribute("type", "button");
+  beginBtnEl.textContent = "Start Quiz";
+  startPageEl.appendChild(beginBtnEl);
+
+  mainEl.appendChild(startPageEl);
 }
 
 function buildQuizPage(questionNum) {
-  //h1El.innerHTML = quizQuestions[questionNum].question;
-  questionEl.textContent = quizQuestions[questionNum].question;
+  startPageEl.classList.add("hide");
+  questionEl.classList.remove("hide");
+  // optionContainerEL.removeAttribute("class", "hide");
+  h1El.textContent = quizQuestions[questionNum].question;
+
+  //questionEl.textContent = quizQuestions[questionNum].question;
   opt1El.textContent = quizQuestions[questionNum].answers[0];
   opt2El.textContent = quizQuestions[questionNum].answers[1];
   opt3El.textContent = quizQuestions[questionNum].answers[2];
   opt4El.textContent = quizQuestions[questionNum].answers[3];
 
-  mainEl.appendChild(h1El);
-  h1El.appendChild(questionEl);
-  optionContainerEL.appendChild(opt1El);
-  optionContainerEL.appendChild(opt2El);
-  optionContainerEL.appendChild(opt3El);
-  optionContainerEL.appendChild(opt4El);
-  mainEl.appendChild(optionContainerEL);
-
+  questionEl.appendChild(h1El);
+  // optionContainerEL.appendChild(opt1El);
+  // optionContainerEL.appendChild(opt2El);
+  // optionContainerEL.appendChild(opt3El);
+  // optionContainerEL.appendChild(opt4El);
+  questionEl.appendChild(opt1El);
+  questionEl.appendChild(opt2El);
+  questionEl.appendChild(opt3El);
+  questionEl.appendChild(opt4El);
+  // questionEl.appendChild(optionContainerEL);
+  mainEl.appendChild(questionEl);
   if (questionNum != 0) {
     footerEl.textContent = check;
     if (questionNum > 5) {
-      clearPage();
       buildScoreFormPage();
     }
   }
 }
 
 function buildScoreFormPage() {
-  questionEl.textContent = "All Done!";
-  pEl.textContent = `Your final score is: ${timer}`;
-  h1El.appendChild(questionEl);
-  mainEl.appendChild(h1El);
-  mainEl.appendChild(pEl);
+  questionEl.classList.add("hide");
+  scoreSubmitEl.classList.remove("hide");
+  h1El.textContent = "All Done!";
+
+  yourScoreEl.textContent = `Your final score is: ${timer}`;
+  scoreSubmitEl.appendChild(h1El);
+  scoreSubmitEl.appendChild(yourScoreEl);
+
+  enterScoreFormEl.textContent = "Enter Inits:";
+
+  inputFieldEl.setAttribute("type", "text");
+  inputFieldEl.setAttribute("name", "score");
+  inputFieldEl.setAttribute("id", "score");
+  inputFieldEl.setAttribute("placeholder", "Your Initials");
+
+  enterScoreFormEl.appendChild(inputFieldEl);
+
+  submitButtonEl.setAttribute("type", "button");
+  submitButtonEl.textContent = "Submit";
+
+  enterScoreFormEl.appendChild(submitButtonEl);
+
+  scoreSubmitEl.appendChild(enterScoreFormEl);
+  mainEl.appendChild(scoreSubmitEl);
+  console.log(inputFieldEl.value);
 }
+
+function buildHighScorePage(score) {
+  scoreSubmitEl.classList.add("hide");
+
+  footerEl.classList.add("hide");
+  highScorePageEl.classList.remove("hide");
+  h1El.textContent = "High Score";
+  highScorePageEl.appendChild(h1El);
+
+  scoreItemEl.appendChild(scoreInfoEl);
+  scoreSheetEl.appendChild(scoreItemEl);
+
+  highScorePageEl.appendChild(scoreSheetEl);
+  goBackButtonEl.textContent = "Go back";
+  clearScores.textContent = "Reset High Scores";
+  highScorePageEl.appendChild(goBackButtonEl);
+  highScorePageEl.appendChild(clearScores);
+  mainEl.appendChild(highScorePageEl);
+
+  for (var i = 0; i < highScores.length; i++) console.log(highScores[i]);
+}
+
+function compareScores() {}
 
 function setTimer() {
   timeInterval = setInterval(function () {
@@ -127,7 +232,7 @@ function setTimer() {
 
 function startGame() {
   setTimer();
-  buildQuizPage(questionNumber);
+  buildStartPage();
 }
 
 function checkAnswer(choice) {
@@ -144,38 +249,61 @@ function checkAnswer(choice) {
   if (questionNumber < quizQuestions.length) {
     buildQuizPage(questionNumber);
   } else {
-    clearPage();
     buildScoreFormPage();
   }
 }
 
-// submitButtonEl.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   var userScores = {
-//     initials: inputFieldEl.value.trim(),
-//     score: timer,
-//   };
+function saveScore() {
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+}
 
-//   createScore(userScores);
+function loadScores() {
+  var highScores = localStorage.getItem("highScores");
 
-//   // userScores.id = scoreNumber;
-//   // highScores.push(userScores);
-//   // scoreNumber++;
-//   // saveScores();
-//   clearPage();
-//   viewHighScores();
-// });
+  if (!highScores) {
+    return false;
+  }
+  highScores = JSON.parse(highScores);
 
-optionContainerEL.addEventListener("click", function (event) {
+  // for (var i = 0; i < highScores.length; i++) {
+  //   console.log(highScores[i]);
+  //   createScore(highScores[i]);
+  // }
+}
+
+//..submitButtonEl.addEventListener("click", scoreFormHandler);
+
+questionEl.addEventListener("click", function (event) {
   var element = event.target;
   var choice;
   if (element.matches("button")) {
     choice = element.textContent;
   }
   checkAnswer(choice);
+  console.log(element);
 });
 
-// submitButtonEl.addEventListener("click", scoreFormHandler);
-// beginBtnEl.addEventListener("click", startGame);
+submitButtonEl.addEventListener("click", scoreFormHandler);
+
+beginBtnEl.addEventListener(
+  "click", //buildQuizPage(questionNumber)
+  function () {
+    console.log(beginBtnEl.value);
+    buildQuizPage(questionNumber);
+  }
+);
+
+goBackButtonEl.addEventListener("click", function () {
+  timer = 30;
+  questionNumber = 0;
+  //goBackButtonEl.setAttribute("class", "hide");
+  //clearScores.setAttribute("class", "hide");
+  //scoreItemEl.setAttribute("class", "hide");
+  startPageEl.classList.remove("hide");
+  while (mainEl.firstChild) {
+    mainEl.removeChild(mainEl.lastChild);
+  }
+  buildQuizPage(questionNumber);
+});
 
 startGame();
